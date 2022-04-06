f0513fa59f4deeb49cd06b30916b83ebfb238c3d
Chidera Ozoh <Chideraozoh@icloud.com>
2022-04-06 17:31:37 +0100

    Created database, tables and added data to tables

--- sql_queries.py
+++ sql_queries.py
@@ -1,49 +1,58 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(songplay_id int PRIMARY KEY, 
+                            start_time int, user_id varchar, level varchar, 
+                            song_id varchar NOT NULL, artist_id varchar NOT NULL, session_id int NOT NULL,
+                            location varchar, userAgent varchar)
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY, first_name varchar, last_name varchar, gender varchar,level varchar)
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, title varchar, artist_id varchar, year int, duration int)
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, name varchar, location varchar, latitude int, longitude int)
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time int, hour int, day int, month int, year int, weekday int)
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, 
+                            song_id, artist_id, session_id, location, userAgent) 
+                            VALUES ()
 """)
 
-user_table_insert = ("""
+user_table_insert = (""" INSERT INTO users (user_id, first_name, last_name, gender,level)
+                         VALUES ()
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
+                        VALUES (%s,%s,%s,%s,%s)
 """)
 
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude)
+                          VALUES ()
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, month, year, weekday)
+                        VALUES ()
 """)
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT * FROM song_table WHERE song_id = "SOCIWDW12A8C13D406"
 """)
 
 # QUERY LISTS
